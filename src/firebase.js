e16e38ddf08b5025ef5dc60ac67ce6bac9a453c7
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-09-25 16:30:50 +0500

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCC5wdhqgpdB5WCq9q0YJu9oMnVUqSlEdw",
+    authDomain: "facebook-clone-e4281.firebaseapp.com",
+    databaseURL: "https://facebook-clone-e4281.firebaseio.com",
+    projectId: "facebook-clone-e4281",
+    storageBucket: "facebook-clone-e4281.appspot.com",
+    messagingSenderId: "509110405047",
+    appId: "1:509110405047:web:3f9416d72677b5a87f4a32"
+};
