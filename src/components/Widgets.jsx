07eb63c637dfead2df44f1ca1c97deaf6168a9ea
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-09-25 23:00:33 +0500

    Facebook updated

--- src/components/Widgets.jsx
+++ src/components/Widgets.jsx
@@ -5,7 +5,7 @@ export default function Widgets() {
   return (
     <div className="widgets">
       <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=187653995174997"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=187653995174997"
         width="340"
         height="100%"
         style={{ border: "none", overflow: "hidden" }}
